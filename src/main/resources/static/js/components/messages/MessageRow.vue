cd138cc4f804e1b47f2558126409001245da8f48
nike <bug.66.910@gmail.com>
2020-09-19 12:20:11 +0500

    front

--- src/main/resources/static/js/components/messages/MessageRow.vue
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
-    <i>({{ message.id }})</i>{{ message.text }}
-    <span style="position: absolute; right: 0;">
-            <input type="button" value="Edit" @click="edit" />
-            <input type="button" value="X" @click="del" />
-        </span>
-    </div>
+    <v-card class="my-2">
+        <v-card-text primary-title>
+            <i>({{ message.id }})</i>
+            {{ message.text }}
+        </v-card-text>
+    <v-card-actions>
+        <v-btn value="Edit" @click="edit" text small>Edit</v-btn>
+        <v-btn @click="del" text small>X</v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <script>
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,14 +2,20 @@ import Vue from 'vue'
 import App from 'pages/App.vue'
 import VueResource from 'vue-resource'
 import { connect } from "./util/ws";
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+
 
 if (frontendData.profile) {
     connect();
 }
-
+Vue.use(Vuetify)
 Vue.use(VueResource)
 
 new Vue({
     el: '#app',
-    render: a => a(App)
+    render: a => a(App),
+    vuetify: new Vuetify()
+
 })
